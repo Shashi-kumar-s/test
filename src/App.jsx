1ac7867ca0de66c0629575d9c6288c90a9e7baa4
Shashi <shashikumarsharma59@gmail.com>
2023-09-30 19:53:25 +0530

    test4

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <p>helllooooooooooooooooooo js</p>
-        <p>heeeeeeeeeeeeeellllllllllllllooooooooo 2</p>
+        <p>helllooooooooooooooooooo js2</p>
+        <p>heeeeeeeeeeeeeellll0</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
